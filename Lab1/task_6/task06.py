ad0f5b27a4e0bbdb4a32a6943f02a5d21072dc84
Constantin Denis <denis.io.constantin@gmail.com>
2023-10-29 13:25:32 +0200

    Update task06.py

--- Lab1/task_6/task06.py
+++ Lab1/task_6/task06.py
@@ -1,14 +1,14 @@
 def func(size):
-
-    romb = ""
-    size += 2 + (size + 1) % 2
-    ################### TO DO #########################
     
+    romb = ""
+    size = size + 2 + (size+1) % 2  #adjust size for easier use
+    ################### TO DO #########################
+
     puncte = ""
     spatii = ""
     for i in range(size//2):
         spatii += " "
-        puncte += ".."
+        puncte += ".." #point and space vector for string splicing
     for i in range(size//2):
         romb += spatii[:size//2-1-i] + "@" + puncte[:-2*(size//2) + 2*i] + "@\n"
     for i in range(size//2-2, -1, -1):
