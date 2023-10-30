2bcaf946670b6a25414b08c02998ee886a785fb7
Constantin Denis <denis.io.constantin@gmail.com>
2023-10-30 23:37:25 +0200

    Update task03.py

--- Lab2/task_3/task03.py
+++ Lab2/task_3/task03.py
@@ -11,10 +11,11 @@ def task(register):
     #     return float(sum(list)/len(list))
     # names = [key for key, value in register.items() if avg(value) >= 8.5]
     #functional approach
-    print(register)
+    #print(register)
+    
     medie = lambda key: float(sum(register[key])/len(register[key])) >= 8.5
-
     names = list(filter(medie, register.keys()))
+    
     #print(names)
     ###################################################
     
